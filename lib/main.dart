9f028fae51b01be5ed4076e0b4e74b439d861570
dim4 <@>
2024-11-02 23:48:54 +0600

    ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:currexch/screens/exchange.dart';
 import 'package:flutter/material.dart';
 
+import 'helpers/colors.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -8,14 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Currexch',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.primaryColor,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          )
+        ),
       ),
       home: const Exchange(),
     );
